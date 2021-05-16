1678625493d36ec8047476b82407fa6da14739ef
RezeR1320 <artem.gromyko.00@gmail.com>
2021-05-16 22:10:49 +0300

    add project configuration

---
+++ Reviews/Extensions/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reviews.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => 
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+        public static void ConfigureIISConfiguration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options => { });
+    }
+}
